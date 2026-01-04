6c00fba27237838e1abb3cb538b480c8dc24e349
ZainBashir29 <zainbashir925@gmail.com>
2026-01-03 23:39:04 -0800

    do a closure question

--- js-mastery/closures.js
+++ js-mastery/closures.js
@@ -13,3 +13,15 @@ c1.increment(); // 12
 
 const c2 = createCounter(0);
 c2.increment(); // 1
+
+function setup() {
+  let value = 0;
+
+  return [() => value++, () => ++value, () => value];
+}
+
+const [a, b, c] = setup();
+
+console.log(a()); // 0
+console.log(b()); // 2
+console.log(c()); // 2
