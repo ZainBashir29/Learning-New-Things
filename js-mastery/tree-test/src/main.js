b567a1e13bd66d3632223c2dfbb4bad8ba003ebf
ZainBashir29 <zainbashir925@gmail.com>
2026-01-05 21:44:29 -0800

    test tree shaking using vite bundler

---
+++ js-mastery/tree-test/src/main.js
@@ -0,0 +1,4 @@
+// main.js
+import { used } from "./utils.js";
+
+used();
---
+++ js-mastery/tree-test/src/utils.js
@@ -0,0 +1,8 @@
+// utils.js
+export function used() {
+  console.log("Used function");
+}
+
+export function unused() {
+  console.log("Unused function");
+}
