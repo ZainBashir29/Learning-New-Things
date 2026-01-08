d43e322c6c32d3f46f649c0c6eec496caecc5b1a
ZainBashir29 <zainbashir925@gmail.com>
2026-01-07 21:18:23 -0800

    do a challenge

--- js-mastery/debounce_and_throttle.js
+++ js-mastery/debounce_and_throttle.js
@@ -18,11 +18,32 @@ function throttle(fn, limit) {
   let inThrottle = false;
 
   return function (...args) {
-    fn.apply(this, args);
-    inThrottle = true;
+    if (!inThrottle) {
+      fn.apply(this, args);
+      inThrottle = true;
+    }
 
     setTimeout(() => {
       inThrottle = false;
     }, limit);
+  };
+}
+
+// Challenge (Leading-edge debouncer)
+
+function leadingEdgeDebounce(fn, delay, immediate = false) {
+  let timer;
+
+  return function (...args) {
+    const callNow = immediate && !timer;
+
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      timer = null;
+      if (!immediate) fn.apply(this, args);
+    }, delay);
+
+    if (callNow) fn.apply(this, args);
   };
 }
