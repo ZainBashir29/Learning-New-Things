5f4134a14402b0a5930113226813d14b13251abf
ZainBashir29 <zainbashir925@gmail.com>
2026-01-10 19:00:07 -0800

    improve validation error handling

--- js-mastery/form_validation.js
+++ js-mastery/form_validation.js
@@ -13,22 +13,40 @@ form.addEventListener("submit", (e) => {
 
   const inputs = form.querySelectorAll("input");
 
-  let isValid = true;
+  // let isValid = true;
+  let isInvalid = null;
 
   inputs.forEach((input) => {
     if (!input.checkValidity()) {
       showError(input);
-      isValid = false;
+      if (!isInvalid) isInvalid = input;
     }
   });
 
-  if (isValid) {
-    console.log("Form submitted");
-    form.reset();
-    // const errorSpans = form.querySelectorAll('[role="alert"]');
-    // errorSpans.forEach((span) => (span.textContent = ""));
+  if (isInvalid) {
+    isInvalid.focus();
+    return;
   }
+
+  // if (isValid) {
+  console.log("Form submitted");
+  form.reset();
+  // const errorSpans = form.querySelectorAll('[role="alert"]');
+  // errorSpans.forEach((span) => (span.textContent = ""));
+  // }5
 });
+
+// const showError = (input) => {
+//   const errorSpan = document.getElementById(`${input.id}-error`);
+
+//   if (input.validity.valid) {
+//     errorSpan.textContent = "";
+//     input.removeAttribute("aria-invalid");
+//   } else {
+//     errorSpan.textContent = input.validationMessage;
+//     input.setAttribute("aria-invalid", true);
+//   }
+// };
 
 const showError = (input) => {
   const errorSpan = document.getElementById(`${input.id}-error`);
@@ -36,15 +54,24 @@ const showError = (input) => {
   if (input.validity.valid) {
     errorSpan.textContent = "";
     input.removeAttribute("aria-invalid");
-  } else {
-    errorSpan.textContent = input.validationMessage;
-    input.setAttribute("aria-invalid", true);
+    input.setCustomValidity("");
+    return;
   }
+
+  let message = input.validationMessage;
+
+  if (input.id === "password" && input.validity.tooShort) {
+    message = "Password must be at least 6 charcters long";
+  }
+
+  errorSpan.textContent = message;
+  input.setCustomValidity(message);
+  input.setAttribute("aria-invalid", "true");
 };
 
-const passwordInput = document.getElementById("password");
-if (passwordInput.length < 6) {
-  const passErrorSpan = form.querySelector("#password-error");
-  passErrorSpan.textContent =
-    "Password should be equal to 6 or more characters";
-}
+// const passwordInput = document.getElementById("password");
+// if (passwordInput.length < 6) {
+//   const passErrorSpan = form.querySelector("#password-error");
+//   passErrorSpan.textContent =
+//     "Password should be equal to 6 or more characters";
+// }
